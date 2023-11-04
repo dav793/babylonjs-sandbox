aac37bcb990289bacfa49770cb2d207961af4744
David Vargas <dav793@gmail.com>
2023-11-03 22:34:04 -0600

    Fix style error + add hemispheric light

--- client/src/app/@modules/engine/engine.service.ts
+++ client/src/app/@modules/engine/engine.service.ts
@@ -9,6 +9,7 @@ import { Scene } from '@babylonjs/core/scene';
 import { MeshBuilder } from '@babylonjs/core/Meshes';
 import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
 import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera';
+import { HemisphericLight } from '@babylonjs/core';
 import { Vector3 } from '@babylonjs/core';
 import { Inspector } from '@babylonjs/inspector';
 
@@ -18,12 +19,16 @@ export class EngineService {
   engine: Engine;
   scene: Scene;
 
+  private _canvas: ElementRef<HTMLCanvasElement>;
+  private _isRunningEngine = false;
+
   constructor() { }
 
   setupEngine(canvas: ElementRef<HTMLCanvasElement>) {
 
     this.engine = new Engine(canvas.nativeElement, true);
     this.scene = this.createScene(this.engine);
+    this._canvas = canvas;
 
     this.runExamples();
 
@@ -38,6 +43,10 @@ export class EngineService {
   }
 
   startRenderLoop() {
+    if (this._isRunningEngine)
+      return; 
+    this._isRunningEngine = true;
+
     var scene = this.scene;
 
     this.engine.runRenderLoop(function() {
@@ -54,10 +63,18 @@ export class EngineService {
     // use default camera + light
     // this.scene.createDefaultCameraOrLight(true, false, true);
     
-    // use universal camera + default light
-    this.scene.createDefaultLight();
+    // use default light
+    // this.scene.createDefaultLight();
+
+    // use hemispheric light
+    const light = new HemisphericLight("mylight", new Vector3(1, 1, 1), this.scene);
+    light.intensity = 0.7;
+
+    // use universal camera
     const camera = new UniversalCamera('mycamera', new Vector3(0, 5, -10), this.scene);
-    camera.attachControl();
+    // camera.rotation.x = 0.5;         // set rotation manually...
+    camera.setTarget(Vector3.Zero());   // or set rotation using a target (here, the scene origin)
+    camera.attachControl(this._canvas.nativeElement, true);
 
     // create a box
     // const box = MeshBuilder.CreateBox("mybox", {}, this.scene);
