9c5f8fd0ad7c427cb154f067dc396a6b270ebc7e
David Vargas <dav793@gmail.com>
2024-01-05 20:24:21 -0600

    Save progress

--- client/src/app/@modules/engine/engine.service.ts
+++ client/src/app/@modules/engine/engine.service.ts
@@ -60,12 +60,6 @@ export class EngineService {
 
   runExamples() {
 
-    // use default camera + light
-    // this.scene.createDefaultCameraOrLight(true, false, true);
-    
-    // use default light
-    // this.scene.createDefaultLight();
-
     // use hemispheric light
     const light = new HemisphericLight("mylight", new Vector3(1, 1, 1), this.scene);
     light.intensity = 0.85;
@@ -76,38 +70,7 @@ export class EngineService {
     camera.wheelPrecision = 100;
     camera.attachControl(this._canvas.nativeElement, true);
 
-    // see tiled ground example: https://doc.babylonjs.com/features/featuresDeepDive/mesh/creation/set/tiled_ground
-    // https://playground.babylonjs.com/#8VDULN#1
-
-    const grid = {
-      'h' : 8,
-      'w' : 8
-    };
-    const tiledGround = MeshBuilder.CreateTiledGround("tiled ground", {xmin: -3, zmin: -3, xmax: 3, zmax: 3, subdivisions: grid}, this.scene);
-
-    const grassMaterial = new StandardMaterial("grass");
-    grassMaterial.diffuseTexture = new Texture("/assets/art/textures/grass.png");
-
-    const rockMaterial = new StandardMaterial("rock");
-    rockMaterial.diffuseTexture = new Texture("/assets/art/textures/rock.png");
-
-    const multimat = new MultiMaterial("multi", this.scene);
-    multimat.subMaterials.push(grassMaterial);
-    multimat.subMaterials.push(rockMaterial);
-
-    tiledGround.material = multimat;
-
-    const verticesCount = tiledGround.getTotalVertices();
-    const tileIndicesLength = tiledGround.getIndices().length / (grid.w * grid.h);
-    tiledGround.subMeshes = [];
-
-    let base = 0;
-    for (let row = 0; row < grid.h; row++) {
-        for (let col = 0; col < grid.w; col++) {
-            tiledGround.subMeshes.push( new SubMesh(row%2 ^ col%2, 0, verticesCount, base , tileIndicesLength, tiledGround) );
-            base += tileIndicesLength;
-        }
-    }
+    
 
   }
 
